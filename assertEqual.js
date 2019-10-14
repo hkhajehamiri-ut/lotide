f519c543bddd5f4255e9a5383b9b63727c356630
hildakh <h_khajehamiri@yahoo.com>
2019-10-14 23:36:21 +0000

    defined and checked an assert equal

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+let assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✔️Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("💣Assertion failed: " + actual + "!== " + expected);
+  }
+};
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Iman", "Hilda");
+assertEqual("Hilda", "Hilda");
+assertEqual(5, 8);
