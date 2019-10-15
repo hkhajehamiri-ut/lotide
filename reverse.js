37ab1f28ffecc7cafaedc8dcb322e17f696bbbc2
hildakh <h_khajehamiri@yahoo.com>
2019-10-15 05:41:59 +0000

    added comments

--- reverse.js
+++ reverse.js
@@ -1,11 +1,11 @@
 let args = process.argv.slice(2);
 let reverse = function(argu) {
   let result = "";
-  for(let i = 0; i < argu.length; i++) {
-    for(let j = argu[i].length-1; j >= 0; j--){
-      result += argu[i].charAt(j);
+  for(let i = 0; i < argu.length; i++) {  //looping through the arguments
+    for(let j = argu[i].length-1; j >= 0; j--){  //looping through the letters of each argument
+      result += argu[i][j];     //adding the letters in reverse to the result string
    }   
-   result += "\n";
+   result += "\n";      // starting the reversed argument on a new line
   } 
   return result;
   
