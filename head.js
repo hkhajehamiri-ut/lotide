639471dac0108056deb645dc42a2133c67b190a8
hildakh <h_khajehamiri@yahoo.com>
2019-10-15 00:48:22 +0000

    just few fixes

--- head.js
+++ head.js
@@ -1,9 +1,9 @@
 let assertEqual = function(actual, expected) {
     if (actual === expected) {
-      console.log(`✔️Assertion Passed: ${actual} === ${expected}`);
+    console.log(`✔️Assertion Passed: ${actual} === ${expected}`);
     } else {
       console.log(`💣Assertion failed: ${actual} !== ${expected}`);
-    }
+  	}
   };
   assertEqual("Lighthouse Labs", "Bootcamp");
   assertEqual(1, 1);
