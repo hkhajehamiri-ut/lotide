b15deac797cbd8238b466f4e7a238a7c5bf68783
hildakh <h_khajehamiri@yahoo.com>
2019-10-16 22:26:02 +0000

    updated eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -24,8 +24,9 @@ const eqArrays = function (arrayOne, arrayTwo) {
 
   }
 }
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
-
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+/*
 console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
 console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+*/
