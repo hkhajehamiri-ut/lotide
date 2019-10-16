81ab6b4f6f918043bcb6c289c645bcba9eda8ce4
hildakh <h_khajehamiri@yahoo.com>
2019-10-16 02:18:46 +0000

    Wrote a function to compare two arrays

---
+++ eqArrays.js
@@ -0,0 +1,31 @@
+let assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✔️Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`💣Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Iman", "Hilda");
+assertEqual("Hilda", "Hilda");
+assertEqual(5, 8);
+
+const eqArrays = function (arrayOne, arrayTwo) {
+  if (arrayOne.length !== arrayTwo.length) {
+    return false;
+  } else {
+    for (let i = 0; i < arrayOne.length; i++) {
+      if (arrayOne[i] !== arrayTwo[i]) {
+        return false;
+      }
+
+    } return true;
+
+  }
+}
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
