fb5440c6d60a8ac5150c93d682da6aa4d6adaf80
hildakh <h_khajehamiri@yahoo.com>
2019-10-18 01:37:09 +0000

    removed unused lines and logs from previous stages

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -5,19 +5,11 @@ let assertEqual = function(actual, expected) {
     console.log(`💣Assertion failed: ${actual} !== ${expected}`);
   }
 };
-/* assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("Iman", "Hilda");
-assertEqual("Hilda", "Hilda");
-assertEqual(5, 8);
-*/
 
 const findKeyByValue = function(objectToCheck, valueToCheck) {
   for(var keyCheck in objectToCheck) {
     if(objectToCheck[keyCheck] == valueToCheck) {
     return keyCheck;
-    
-      //  return key;
     } 
   }
 }
