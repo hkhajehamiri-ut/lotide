e4e0a58a7a9ce27c04ee46ba0a090147d2095955
hildakh <h_khajehamiri@yahoo.com>
2019-10-21 22:15:12 +0000

    refactored

--- middle.js
+++ middle.js
@@ -1,21 +1,20 @@
-
 const middle = function (array) {
   let midElement = [];
-
-  if (array.length === 1 && array.length === 2) return midElement; // instead array.length <= 2
-
-  if (array.length % 2 === 0) {
+  
+  if (array.length < 3) {
+  return midElement; // instead array.length <= 2
+  } else if (array.length % 2 === 0) {
     midElement.push(array[array.length / 2 - 1]);
     midElement.push(array[array.length / 2]);
-  } 
-  // the second if can be an else if as it is impossible for both conditions to be true 
 
-  if (array.length % 2 === 1) {
+  } else if (array.length % 2 === 1) {
+     // the second if can be an else if as it is impossible for both conditions to be true 
     midElement.push(array[(array.length - 1) / 2]);
   }
-
+  
   return midElement;
 }
+
 
 
 module.exports = middle;
