5749d2eb14285f37c8f828e560f346274c0c955d
hildakh <h_khajehamiri@yahoo.com>
2019-10-21 19:53:04 +0000

    refactored the tests and separated the test cases from functions

---
+++ test/tailTest.js
@@ -0,0 +1,15 @@
+const assertEqual = require('../assertEqual');
+const tail = require('../tail');
+
+let result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2);
+assertEqual(result[0], "Lighthouse");
+assertEqual(result[1], "Labs");
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
+
+let emptyArray = [];
+tail(emptyArray);
+assertEqual(emptyArray.length, 0);
