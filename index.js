d6ca7128a60381f3a9f18acfabba14339f40d279
hildakh <h_khajehamiri@yahoo.com>
2019-10-21 23:24:41 +0000

    index updated using implement object shorthand

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 // index.js
-// const assertArraysEqual   = require('./assertArraysEqual');
-// const assertEqual = require('./assertEqual');
-// const assertObjectEqualhead   = require('./assertObjectEqual');
+const assertArraysEqual   = require('./assertArraysEqual');      // might need to remove them later
+const assertEqual = require('./assertEqual');                    // might need to remove them later
+const assertObjectEqual  = require('./assertObjectEqual');    // might need to remove them later
 const countLetters   = require('./countLetters');
 const countOnly   = require('./countOnly');
-// const eqArrays   = require('./eqArrays');
-const eqObjects   = require('./eqObjects');
+const eqArrays   = require('./eqArrays');     // might need to remove them later
+const eqObjects   = require('./eqObjects');    // might need to remove them later
 const findKey   = require('./findKey');
 const findKeyByValue   = require('./findKeyByValue');
 const flatten   = require('./flatten');
 const head   = require('./head');
-const letterPosition   = require('./letterPosition');
+const letterPositions   = require('./letterPositions');
 const map   = require('./map');
 const middle = require('./middle');
 const tail   = require('./tail');
@@ -18,22 +18,22 @@ const takeUntil   = require('./takeUntil');
 const without   = require('./without');
 
 module.exports = {
-  // assertArraysEqual: assertArraysEqual,
-  // assertEqual: assertEqual,
-  // assertObjectEqual: assertObjectEqual,
-  countLetters: countLetters,
-  countOnly: countOnly,
-  // eqArrays: eqArrays,
-  // eqObjects: eqObjects,
-  findKey: findKey,
-  findKeyByValue: findKeyByValue,
-  flatten: flatten,
-  head:   head,
-  letterPosition: letterPosition,
-  map: map,
-  middle: middle,
-  tail:   tail,
-  takeUntil: takeUntil,
-  without: without
+  assertArraysEqual,   // might need to remove them later
+  assertEqual,               // might need to remove them later
+  assertObjectEqual,   // might need to remove them later
+  countLetters,
+  countOnly,
+  eqArrays,   // might need to remove them later
+  eqObjects,  // might need to remove them later
+  findKey,
+  findKeyByValue,
+  flatten,
+  head,
+  letterPositions,
+  map,
+  middle,
+  tail,
+  takeUntil,
+  without
 
 };
