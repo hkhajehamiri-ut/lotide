0b5db3b8a690d22bf952b694f04a1e9663e4ba28
hildakh <h_khajehamiri@yahoo.com>
2019-10-21 05:54:09 +0000

    Adding nested arrays to the top level using loops

---
+++ flatten.js
@@ -0,0 +1,58 @@
+const eqArrays = function (arrayOne, arrayTwo) {
+  if (arrayOne.length !== arrayTwo.length) {
+    return false;
+  } else {
+    for (let i = 0; i < arrayOne.length; i++) {
+      if (arrayOne[i] !== arrayTwo[i]) {
+        return false;
+      }
+
+    } return true;
+  }
+}
+
+const assertArrayEqual = function (arrOne, arrTwo) {
+  if (eqArrays(arrOne, arrTwo) === true) {
+    console.log(`Arrays are equal: ${arrOne} === ${arrTwo}`);
+  } else {
+    console.log(`Arrays are not equal: ${arrOne} !== ${arrTwo}`);
+  }
+}
+
+
+const flatten = function (array) {
+  let flattenedArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])) {
+      for (let j = 0; j < array[i].length; j++) {
+        flattenedArray.push(array[i][j]);
+      } 
+    }
+      else {
+        flattenedArray.push(array[i]);
+      }
+    }
+  
+  return flattenedArray;
+};
+
+assertArrayEqual(flatten(["hilda", "iman", ["mina", "jahan"], ["armin"]]), ["hilda", "iman", "mina", "jahan", "armin"]);
+
+assertArrayEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
+
+
+
+//Ask why recursion won't work here
+// const flatten = function(array) {
+//   let flattenedArray = [];
+//   for(item of array) {
+//     if (Array.isArray(item)) {
+//       flatten(item);
+//     } else {
+//       flattenedArray.push(item);
+//     }
+//   }
+//   return flattenedArray;
+// }
+
+// flatten([1, 2, [3, 4], 5, [6]]);
