2604989bd86ec074a0d3668e2330afd9e2d973c3
hildakh <h_khajehamiri@yahoo.com>
2019-10-21 20:11:06 +0000

    refactored

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,36 +1,6 @@
-let assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✔️Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`💣Assertion failed: ${actual} !== ${expected}`);
-  }
-};
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("Iman", "Hilda");
-assertEqual("Hilda", "Hilda");
-assertEqual(5, 8);
+const eqArrays = require('./eqArrays');
 
-const eqArrays = function (arrayOne, arrayTwo) {
-  if (arrayOne.length !== arrayTwo.length) {
-    return false;
-  } else {
-    for (let i = 0; i < arrayOne.length; i++) {
-      if (arrayOne[i] !== arrayTwo[i]) {
-        return false;
-      }
-
-    } return true;
-
-  }
-}
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
-
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
-
-const assertArrayEqual = function (arrOne, arrTwo) {
+const assertArraysEqual = function (arrOne, arrTwo) {
   if (eqArrays(arrOne, arrTwo) === true) {
     console.log(`Arrays are equal: ${arrOne} === ${arrTwo}`);
   } else {
@@ -38,5 +8,4 @@ const assertArrayEqual = function (arrOne, arrTwo) {
   }
 } 
 
-assertArrayEqual(["1", "2", "3"], ["1", "2", "3"]);
-assertArrayEqual(["1", "2", "3"], ["1", "2", 3]);
+module.exports = assertArraysEqual;
