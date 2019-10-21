3d560d5cb4ad80d3ec3cca25931b29e3f1ac3767
hildakh <h_khajehamiri@yahoo.com>
2019-10-21 22:25:55 +0000

    refactored

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,15 +1,45 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-let result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
+describe("#middle", () => {
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+  it("returns [7, 3] for [5, 7, 3]", () => {
+    assert.deepEqual(tail([5, 7, 3]), [7, 3]); 
+  });
 
-let emptyArray = [];
-tail(emptyArray);
-assertEqual(emptyArray.length, 0);
+  it("returns [] for []", () => {
+    assert.deepEqual(tail([]), []); 
+  });
+
+  it("returns [] for [1]", () => {
+    assert.deepEqual(tail([1]), []); 
+  });
+
+  it("returns [2] for [1, 2]", () => {
+    assert.deepEqual(tail([1, 2]), [2]); 
+  });
+
+
+  it("returns [2, 4, 9, 5] for [1, 2, 4, 9, 5]", () => {
+    assert.deepEqual(tail([1, 2, 4, 9, 5]), [2, 4, 9, 5]); 
+  });
+
+  it("returns [9, 5, 4] for [1, 9, 5, 4]", () => {
+    assert.deepEqual(tail([1, 9, 5, 4]), [9, 5, 4]); 
+  });
+
+  it("returns [5, 6, 7, 8, 9] for [4, 5, 6, 7, 8, 9]", () => {
+    assert.deepEqual(tail([4, 5, 6, 7, 8, 9]), [5, 6, 7, 8, 9]); 
+  });
+
+  it("returns ['b', 'c', 'd', 'x'] for ['a', 'b', 'c', 'd', 'x']", () => {
+    assert.deepEqual(tail(['a', 'b', 'c', 'd', 'x']), ['b', 'c', 'd', 'x']); 
+  });
+
+  it("returns ['b', 'c', 'd'] for ['a', 'b', 'c', 'd']", () => {
+    assert.deepEqual(tail(['a', 'b', 'c', 'd']), ['b', 'c', 'd']); 
+  });
+
+});
+
+
